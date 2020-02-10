eb0efe45017ba72f593c272a1ccdd510c0989db9
Bailee Dastugue <bailee.dast@gmail.com>
2020-02-10 09:36:54 -0600

    converted underscore array to string, sep by spaces

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,30 @@
     /* variable to hold number of wins */
     var numWins = 0;
     /* variable to hold words correctly guessed */
-    var correctWords = [];
+    var correctGuesses = [];
     /* variable to hold number of chances to corectly guess */
     var chancesLeft = 10;
 
     var directions = document.getElementById("directions");
     var correctWord = document.getElementById("correct-word");
     
-    var possibleWords = ["apple", "banana", "jack"];
+    var possibleWords = ["apple", "banana", "jack", "texas", "bellatrix"];
     var thisWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
     console.log(possibleWords);
     console.log(thisWord);
 
+
     var numUnderscores = [];
 
-    function underscores (thisWord) {
+    function underscores () {
         var wordLength = thisWord.length;
         console.log(wordLength);
         for (var i = 0; i < wordLength; i++) {
-            numUnderscores.push("_ "); 
+            numUnderscores.push("_"); 
             }
-            console.log(numUnderscores);
+        var strUnderscores = numUnderscores.join(" ");
+        console.log(strUnderscores);
     }
 
-    underscores(thisWord);
+    underscores();
 
