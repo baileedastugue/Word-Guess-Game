8835afe773b7ea55e9b4d42e36ff4cb3cbc346be
Bailee Dastugue <bailee.dast@gmail.com>
2020-02-11 18:38:36 -0600

    if statement - key press shows up in console, replaces underscore

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,15 +11,18 @@
     var directions = document.getElementById("directions");
     var correctWord = document.getElementById("correct-word");
     
-
-    var possibleWords = ["apple", "banana", "jack", "texas", "bellatrix"];
+    // array of possible words to guess
+    var possibleWords = ["educate", "orange", "jack", "texas", "bellatrix"];
+    // randomly pick a word from the array
     var thisWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
     console.log(possibleWords);
     console.log(thisWord);
+    // stores the length of the chosen word 
     var wordLength = thisWord.length;
 
+    // creates an empty array for the number of underscores in an array
     var numUnderscores = [];
-
+    // function that 
     function underscores () {
         for (var i = 0; i < wordLength; i++) {
             numUnderscores.push("_"); 
@@ -27,16 +30,26 @@
         var strUnderscores = numUnderscores.join(" ");
         return strUnderscores;
     }
-
+    
     var num = underscores();
     correctWord.textContent = num;
+    var partialWord = correctWord.textContent;
 
     document.onkeyup = function(event) {
         var userGuess = event.key;
-        for (var i = 0; i < correctWord; i++) {
-            console.log([i]);
-        }
+        // for (var i = 0; i < wordLength; i++){
+            var part1 = partialWord.substr(0,3);
+            var part2 = partialWord.substring(3, partialWord.length-2);
+            if (userGuess ===  thisWord.charAt(2)) {
+                    partialWord = part1 + " " + thisWord.charAt(2) + " " + part2;
+            }
+            console.log(partialWord);
+        // }
+        // correctWord.textContent = partialWord;
     }
+
+
+    
     
 
 
