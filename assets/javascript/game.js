cdd52f3358e4c54ca746da8a9801a93f6f1df159
Bailee Dastugue <bailee.dast@gmail.com>
2020-02-13 09:20:31 -0600

    removed redundant variables, added additional comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,14 +8,13 @@
     var possibleWords = ["educate", "orange", "jack", "texas", "bellatrix"];
     // randomly pick a word from the array
     var thisWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-    console.log(possibleWords);
     console.log(thisWord);
     // stores the length of the chosen word 
     var wordLength = thisWord.length;
 
-    // creates an empty array for the number of underscores in an array
+    // creates an empty array for the number of underscores
     var numUnderscores = [];
-    // function that 
+    // fills underscore array with the length of the word 
     function underscores () {
         for (var i = 0; i < wordLength; i++) {
             numUnderscores.push("_"); 
@@ -24,10 +23,9 @@
     }
 
     var underscoreHolder = underscores();
+    var underscoreArray = Array.from(underscoreHolder);
 
     var wordArray = Array.from(thisWord);
-    var underscoreArray = Array.from(underscoreHolder);
-    var correctArray = Array.from(thisWord);
     
     guessesLeft.textContent = ("Chances left to save chicken little: " + 10);
     
@@ -50,20 +48,20 @@
             guessesLeft.textContent = ("Chances left to save chicken little: " + chancesLeft);
            
         }
+        // updates underscores on page to reflect key strokes
         var strArray = underscoreArray.join(" ");
         correctWord.textContent = strArray;
 
-        
+        // tests whether all letters have been guessed
         if (underscoreArray.includes("_")) {
-            
         }
+        // if all letters have been guessed,
+        // alerts user, includes words in Words Guessed section
         else {
             alert("you won!");
             wordsWon.push(thisWord);
             wordsGuessed.textContent = "Words you've correctly guessed: " + wordsWon;
         }
-        
-        
     }
 
 
